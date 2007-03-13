a4ac0e1d816c3a7efd1857cf2e6c4e28cb8ddfb2
moudrick <moudrick@b2ca9c5d-3321-0410-a35f-a521905b3e2e>
2007-03-13 18:33:32 +0000

    NET_1_1 Rainbow folder filled with danijel.kecman's content - Part 8 - aspnet_client branched similar

---
+++ NET_1_1/trunk/Rainbow/aspnet_client/Rainbow_scripts/confirmDelete.js
@@ -0,0 +1,12 @@
+<script LANGUAGE="JavaScript">
+<!--
+function confirmDelete()
+{
+    var agree=confirm("%0%");
+    if (agree)
+        return true ;
+    else
+        return false ;
+}
+// -->
+</script>
