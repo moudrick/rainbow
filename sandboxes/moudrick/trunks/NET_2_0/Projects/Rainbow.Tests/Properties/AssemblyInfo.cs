90a678628bbe3f202991c9917ed9fc5a32da5816
moudrick <moudrick@b2ca9c5d-3321-0410-a35f-a521905b3e2e>
2007-05-28 00:40:49 +0000

    common VersionInfo.cs for all Rainbow Class Library projects

--- sandboxes/moudrick/trunks/NET_2_0/Projects/Rainbow.Tests/Properties/AssemblyInfo.cs
+++ sandboxes/moudrick/trunks/NET_2_0/Projects/Rainbow.Tests/Properties/AssemblyInfo.cs
@@ -22,14 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "a4383f6d-c241-4d0f-ba4b-59342e7af53d" )]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
