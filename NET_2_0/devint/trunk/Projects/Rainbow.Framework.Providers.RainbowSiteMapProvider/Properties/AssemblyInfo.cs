246487250d79c4d4384191640b4180a0b36ca7e8
moudrick <moudrick@b2ca9c5d-3321-0410-a35f-a521905b3e2e>
2007-06-05 19:35:33 +0000

    nant.build & VersionInfo.cs workflow introduced

--- NET_2_0/devint/trunk/Projects/Rainbow.Framework.Providers.RainbowSiteMapProvider/Properties/AssemblyInfo.cs
+++ NET_2_0/devint/trunk/Projects/Rainbow.Framework.Providers.RainbowSiteMapProvider/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4444d626-2c66-486c-ab2a-c70b3e628cb0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- NET_2_0/devint/trunk/Projects/Rainbow.Framework/Properties/AssemblyInfo.cs
+++ NET_2_0/devint/trunk/Projects/Rainbow.Framework/Properties/AssemblyInfo.cs
@@ -23,16 +23,4 @@ using System.Security;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("03094a42-cc29-4a65-aead-e2f803e90931")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.1880")]
-[assembly: AssemblyFileVersion("2.0.0.1880")]
-
 [assembly: AllowPartiallyTrustedCallers]
