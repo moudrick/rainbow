0f348ef2d1321c0dd3448c7887bc0801e9c582c1
wforney <wforney@b2ca9c5d-3321-0410-a35f-a521905b3e2e>
2007-12-20 18:05:21 +0000

    just moving stuff from core into here.

--- NET_3_5/devint/trunk/Projects/Rainbow.Framework.Configuration/Properties/AssemblyInfo.cs
+++ NET_3_5/devint/trunk/Projects/Rainbow.Framework.Configuration/Properties/AssemblyInfo.cs
@@ -24,16 +24,3 @@ using System;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0a5b6dcb-40eb-4e34-ba3b-457230d74a2e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
