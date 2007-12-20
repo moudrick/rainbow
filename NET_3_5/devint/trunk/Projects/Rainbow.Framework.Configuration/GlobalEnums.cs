22d71a9d35ccc45b7b3451c336daed7011ff3575
wforney <wforney@b2ca9c5d-3321-0410-a35f-a521905b3e2e>
2007-12-20 03:45:45 +0000

    Added Rainbow.Framework.Configuration
    should be mostly .NET v2 friendly.

---
+++ NET_3_5/devint/trunk/Projects/Rainbow.Framework.Configuration/GlobalEnums.cs
@@ -0,0 +1,93 @@
+namespace Rainbow.Framework.Configuration
+{
+    /// <summary>
+    /// Enumeration for workflow version
+    /// by Geert.Audenaert@Syntegra.Com
+    /// Date: 6/2/2003
+    /// </summary>
+    public enum WorkFlowVersion
+    {
+        /// <summary>
+        /// Production
+        /// </summary>
+        Production = 1,
+        /// <summary>
+        /// Staging
+        /// </summary>
+        Staging = 2
+    }
+
+    /// <summary>
+    /// WorkflowState
+    /// by Geert.Audenaert@Syntegra.Com
+    /// Date: 27/2/2003
+    /// </summary>
+    public enum WorkflowState
+    {
+        /// <summary>
+        /// The staging and production content are identical
+        /// </summary>
+        Original = 0,
+        /// <summary>
+        /// Were working on the staging content
+        /// </summary>
+        Working = 1,
+        /// <summary>
+        /// The staging content is ready and approval is been requested
+        /// </summary>
+        ApprovalRequested = 2,
+        /// <summary>
+        /// The staging content is approved and ready to be published
+        /// </summary>
+        Approved = 3
+    }
+}
+
+namespace Rainbow.Framework.Configuration
+{
+    // TODO: Uncomment this for web solution compatibility, 
+    // it was commented to change the core.
+    /*
+	/// <summary>
+	/// Enumeration for workflow version
+	/// by Geert.Audenaert@Syntegra.Com
+	/// Date: 6/2/2003
+	/// </summary>
+	public enum WorkFlowVersion
+	{
+		/// <summary>
+        /// Please use Rainbow.Framework.WorkFlowVersion
+		/// </summary>
+		Production = 1,
+		/// <summary>
+        /// Please use Rainbow.Framework.WorkFlowVersion
+		/// </summary>
+		Staging = 2
+	}
+    /// <summary>
+    /// WorkflowState
+    /// by Geert.Audenaert@Syntegra.Com
+    /// Date: 27/2/2003
+    /// </summary>
+    public enum WorkflowState
+    {
+        /// <summary>
+        /// Please use Rainbow.Framework.WorkflowState
+        /// </summary>
+        Original = 0,
+        /// <summary>
+        /// Please use Rainbow.Framework.WorkflowState
+        /// </summary>
+        Working = 1,
+        /// <summary>
+        /// Please use Rainbow.Framework.WorkflowState
+        /// </summary>
+        ApprovalRequested = 2,
+        /// <summary>
+        /// Please use Rainbow.Framework.WorkflowState
+        /// </summary>
+        Approved = 3
+    }
+
+*/
+}
