8a2e3e8a10a003f937f341a887fdc1e345c7cfda
mkkurz <mkkurz@b2ca9c5d-3321-0410-a35f-a521905b3e2e>
2008-01-05 08:18:32 +0000

    Changed dates to culture independent format.

--- NET_2_0/devint/trunk/WebSites/Rainbow/Setup/Scripts/2.0.0.1881_MembershipRoleProviders.sql
+++ NET_2_0/devint/trunk/WebSites/Rainbow/Setup/Scripts/2.0.0.1881_MembershipRoleProviders.sql
@@ -3636,11 +3636,11 @@ GO
 -------------------------------  USERS   ------------------------------------------
 
 INSERT INTO dbo.aspnet_Users 
-VALUES (N'{CA8DEE6B-0B81-4462-AF3B-6A22B36A0304}', N'{BE7DC028-7238-45D3-AF35-DD3FE4AEFB7E}', N'admin@rainbowportal.net', N'admin@rainbowportal.net', NULL, 0, '2006-04-05 18:12:37.140')
+VALUES (N'{CA8DEE6B-0B81-4462-AF3B-6A22B36A0304}', N'{BE7DC028-7238-45D3-AF35-DD3FE4AEFB7E}', N'admin@rainbowportal.net', N'admin@rainbowportal.net', NULL, 0, '2006-04-05T18:12:37.140')
 go
 
 INSERT INTO dbo.aspnet_Membership(ApplicationId, UserId, Password, PasswordFormat, PasswordSalt, MobilePIN, Email, LoweredEmail, PasswordQuestion, PasswordAnswer, IsApproved, IsLockedOut, CreateDate, LastLoginDate, LastPasswordChangedDate, LastLockoutDate, FailedPasswordAttemptCount, FailedPasswordAttemptWindowStart, FailedPasswordAnswerAttemptCount, FailedPasswordAnswerAttemptWindowStart) 
-VALUES (N'{CA8DEE6B-0B81-4462-AF3B-6A22B36A0304}', N'{BE7DC028-7238-45D3-AF35-DD3FE4AEFB7E}', N'admin', 0, N'', NULL, N'admin@rainbowportal.net', N'admin@rainbowportal.net', N'question', N'answer', 1, 0, '2006-03-23 13:46:25.000', '2006-03-23 21:46:58.203', '2006-03-23 13:46:25.000', '1754-01-01 00:00:00.000', 1, '2006-04-05 18:12:37.110', 0, '1754-01-01 00:00:00.000')
+VALUES (N'{CA8DEE6B-0B81-4462-AF3B-6A22B36A0304}', N'{BE7DC028-7238-45D3-AF35-DD3FE4AEFB7E}', N'admin', 0, N'', NULL, N'admin@rainbowportal.net', N'admin@rainbowportal.net', N'question', N'answer', 1, 0, '2006-03-23T13:46:25.000', '2006-03-23T21:46:58.203', '2006-03-23T13:46:25.000', '1754-01-01T00:00:00.000', 1, '2006-04-05T18:12:37.110', 0, '1754-01-01T00:00:00.000')
 go
 
 INSERT INTO dbo.aspnet_UsersInRoles VALUES (N'{BE7DC028-7238-45D3-AF35-DD3FE4AEFB7E}', N'{F6A4ADDA-8450-4F9A-BE86-D0719B239A8D}')
