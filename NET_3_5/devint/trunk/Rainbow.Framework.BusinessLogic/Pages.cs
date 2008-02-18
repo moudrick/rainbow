41db15b06d19747a891af1573ab361eaecdd7775
wforney <wforney@b2ca9c5d-3321-0410-a35f-a521905b3e2e>
2008-02-18 09:24:03 +0000

    Created business logic project and moved some stuff into it.

---
+++ NET_3_5/devint/trunk/Rainbow.Framework.BusinessLogic/Pages.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rainbow.Framework.Data.Entities;
+using Rainbow.Framework.Data.DataSources;
+
+namespace Rainbow.Framework.Data
+{
+    /// <summary>
+    /// Generic Pages Data Access Class
+    /// </summary>
+    public sealed class Pages
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pages"/> class.
+        /// </summary>
+        private Pages() { }
+
+        /// <summary>
+        /// source provider instance
+        /// </summary>
+        static PageProvider source = PageProvider.Instance();
+
+        /// <summary>
+        /// Add a Page to the data source.
+        /// </summary>
+        /// <param name="Id">The page id.</param>
+        /// <param name="name">The name.</param>
+        public static void Add(Guid id, string name) //add params as appropriate
+        {
+            IPage record = source.CreateNew() as IPage;  //create new entity in memory
+
+            //add values
+            record.Id = id;
+            record.Name = name;
+
+
+            source.Add(record);     //add record to data source in memory
+            source.CommitChanges();     //write changes back to data source
+        }
+
+        public static void Remove(Guid id)
+        {
+            IPage record = source.GetById(id) as IPage;  //grab page from data source as IPage interface object
+
+            source.Remove(record);      //delete record from data source in memory
+            source.CommitChanges();     //write changes back to data source
+        }
+
+        public static List<IPage> LoadAll()
+        {
+            return new List<IPage>(source.GetAll());
+        }
+
+        public static IPage LoadById(Guid id)
+        {
+            return source.GetById(id);
+        }
+    }
+}
---
+++ NET_3_5/devint/trunk/Rainbow.Framework.BusinessLogic/Portals.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rainbow.Framework.Data.Entities;
+using Rainbow.Framework.Data.DataSources;
+using System.Threading;
+using System.Globalization;
+
+namespace Rainbow.Framework.Data
+{
+    /// <summary>
+    /// Generic Portals Data Access Class
+    /// </summary>
+    public sealed class Portals
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Portals"/> class.
+        /// </summary>
+        private Portals() {}
+
+        /// <summary>
+        /// source provider instance
+        /// </summary>
+        static PortalProvider source = PortalProvider.Instance();
+
+        /// <summary>
+        /// Add a Portal to the data source.
+        /// </summary>
+        /// <param name="Id">The portal id.</param>
+        /// <param name="name">The name.</param>
+        public static void Add(Guid id, string alias, CultureInfo contentLanguage, DateTime? createdOn,
+            CultureInfo dataFormattingCulture, bool? isAlwaysShowEditButton, bool? isDeleted, bool? isShowPages,
+            ILayout layout, Guid objectTypeId, string tos, ITheme themePrimary, ITheme themeSecondary,
+            string title, CultureInfo uiLanguage) //add params as appropriate
+        {
+            IPortal record = source.CreateNew() as IPortal;  //create new entity in memory
+
+            //add values
+            record.Id = id;
+            record.Alias = alias;
+            record.ContentLanguage = contentLanguage;
+            record.CreatedOn = createdOn ?? DateTime.Now;
+            record.DataFormattingCulture = dataFormattingCulture;
+            record.IsAlwaysShowEditButton = isAlwaysShowEditButton ?? false;
+            record.IsDeleted = isDeleted ?? false;
+            record.IsShowPages = isShowPages ?? true;
+            record.Layout = layout;
+            record.ObjectTypeId = objectTypeId;
+            record.TermsOfService = tos ?? string.Empty;
+            record.ThemePrimary = themePrimary;
+            record.ThemeSecondary = themeSecondary;
+            record.Title = title ?? string.Empty;
+            record.UILanguage = uiLanguage;
+
+            source.Add(record);     //add record to data source in memory
+            source.CommitChanges();     //write changes back to data source
+        }
+
+        public static void Remove(Guid id)
+        {
+            IPortal record = source.GetById(id) as IPortal;  //grab portal from data source as IPortal interface object
+
+            source.Remove(record);      //delete record from data source in memory
+            source.CommitChanges();     //write changes back to data source
+        }
+
+        public static List<IPortal> LoadAll()
+        {
+            return new List<IPortal>(source.GetAll());
+        }
+
+        public static IPortal LoadById(Guid id)
+        {
+            return source.GetById(id);
+        }
+    }
+}
